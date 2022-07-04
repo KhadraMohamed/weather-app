44fb80b4c0bd76cef953ba7898937fc765450247
khadra mohamed <khadramohamed@live.co.uk>
2022-07-04 17:16:07 +0100

    first testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
+import forecast from "../data/forecast.json";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders App component correctly", () => {
+    render(<App location={forecast.location} forecasts={forecast.forecasts} />);
+    const h1Element = screen.getByText(/Leeds, UK/i);
+    expect(h1Element).toBeInTheDocument();
+  });
 });

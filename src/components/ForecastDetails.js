db4d96293aa15b577716496c84424254656d9e82
khadra mohamed <khadramohamed@live.co.uk>
2022-07-06 00:00:07 +0100

    1.5/6 of Detailed Forecast

---
+++ src/components/ForecastDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ForecastDetails(forecast) {
+  return <div className="forecast-details"> </div>;
+}
+
+export default ForecastDetails;
+
+ForecastDetails.propTypes = {};

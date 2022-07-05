01e59621f15329866477b509786a1ac02b4aabcd
khadra mohamed <khadramohamed@live.co.uk>
2022-07-05 20:43:04 +0100

    Updated Snapshot tests

--- src/tests/ForecastSummaries.test.js
+++ src/tests/ForecastSummaries.test.js
@@ -9,13 +9,13 @@ describe("ForecastSummaries", () => {
       description: "Stub description 1",
       icon: "stubIcon1",
       temperature: {
-        max: 22,
         min: 12,
+        max: 22,
       },
     },
     {
       date: 2222222,
-      description: "Stub description2",
+      description: "Stub description 2",
       icon: "stubIcon2",
       temperature: {
         max: 24,
@@ -23,8 +23,16 @@ describe("ForecastSummaries", () => {
       },
     },
   ];
-});
-it("renders correctly", () => {
-  const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
-  expect(asFragment()).toMatchSnapshot();
+
+  it("renders correctly", () => {
+    const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("renders the correct number of ForecastSummary instances", () => {
+    const { getAllByTestId } = render(
+      <ForecastSummaries forecasts={validProps} />
+    );
+
+    expect(getAllByTestId("forecast-summary")).toHaveLength(2);
+  });
 });

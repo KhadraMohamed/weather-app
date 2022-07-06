23c0cbf4202d6e6b5f0d77817afbb92dc31cca71
khadra mohamed <khadramohamed@live.co.uk>
2022-07-06 16:54:36 +0100

    Detailed Forecast 2.5/6

--- src/components/ForecastDetails.js
+++ src/components/ForecastDetails.js
@@ -2,9 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ForecastDetails(forecast) {
+  const { humidity, direction, windspeed, temperature } = props;
+  const formattedDate = new Date(date).toDateString();
+
   return <div className="forecast-details"> </div>;
 }
 
 export default ForecastDetails;
 
-ForecastDetails.propTypes = {};
+ForecastDetails.propTypes = {
+  formatteddate: PropTypes.number.isRequired,
+  humidity: PropTypes.string.isRequired,
+  direction: PropTypes.string.isRequired,
+  windspeed: PropTypes.string.isRequired,
+  temperature: PropTypes.shape({
+    min: PropTypes.number,
+    max: PropTypes.number,
+  }).isRequired,
+};
